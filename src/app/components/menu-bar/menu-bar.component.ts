9e4585e3382996d6a58c1bd9540377093644277f
heliospjunior <heliospjunior@gmail.com>
2024-02-08 14:17:45 -0300

    components dynamics properties

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrl: './menu-bar.component.css'
+  styleUrl: './menu-bar.component.css',
 })
 export class MenuBarComponent {
 
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit{
+
+  @Input()
+  photoCover:string = "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg";
+  @Input()
+  cardTitle:string = "Nova Série anunciada"
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
